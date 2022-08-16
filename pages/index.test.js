1cae2e8c750c4913ba387c00147ebd3a3244f3ee
adetyaz <tayo.lasisi@yahoo.com>
2022-08-16 16:33:15 +0100

    feat: tests

--- pages/index.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-
-import Home from './index'
-
-test('The Home page should render', () => {
-	const view = render(<Home />)
-	expect(view).toMatchSnapshot()
-})
